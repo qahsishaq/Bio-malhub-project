26d29050cce82bd6090955152b25511b81e6ee98
Ishaq Ishaq <ramadanishaq.7636@gmail.com>
2023-12-27 07:56:46 +0100

    First commit

---
+++ JavaScript/questions.js
@@ -0,0 +1,101 @@
+//creating an array and passing the number , questions , options ,and answers.
+let questions = [
+  {
+    numb: 1,
+    question: "What does CSS stand for?",
+    answer: "Cascading StyleSheet",
+    options: [
+      "Casading StyleSheet",
+      "Calculating StyleSheet",
+      "Cascading StyleSheet",
+      "None of the above",
+    ],
+  },
+
+  {
+    numb: 2,
+    question: "In bootstrap, what does the 'container-fluid' do?",
+    answer: "It fills the screen regardless of any breakpoint",
+    options: [
+      "The container allows different width for each breakpoint",
+      "It fills the container with a water background",
+      "It fills the screen regardless of any breakpoint",
+      "None of the above",
+    ],
+  },
+
+  {
+    numb: 3,
+    question: "What does Javascript do?",
+    answer: "It makes your work interactive",
+    options: [
+      "All of the above",
+      "It makes your work interactive",
+      "It creates elements and various tags",
+      "It styles your webpage",
+    ],
+  },
+  {
+    numb: 4,
+    question: "An Array is another type of ...?",
+    answer: "All of the above",
+    options: ["Object", "Data type", "All of the above", "None of the above"],
+  },
+  {
+    numb: 5,
+    question: "Which of the following is not a Javascript Array method?",
+    answer: "Cascading StyleSheet",
+    options: [".swipe()", ".slice()", ".pop()", ".push()"],
+  },
+  {
+    numb: 6,
+    question: "Images are made up of tiny little boxes known as....?",
+    answer: "Pixels",
+    options: ["Bits", "Dots", "Pixels", "Colored box"],
+  },
+
+  {
+    numb: 7,
+    question:
+      "The address in which every device is recognized over the internet is known as the....?",
+    answer: "IP address",
+    options: [
+      "Email address",
+      "Physical address",
+      "IP address",
+      "Unknown Address",
+    ],
+  },
+  {
+    numb: 8,
+    question:
+      "According to the Javascript syntax, which of these is not an integer? ",
+    answer: "NaN",
+    options: ["NaN", "2", "BigInt", "101"],
+  },
+
+  {
+    numb: 9,
+    question: "A good developer should adopt all these habits except",
+    answer: "Arrogance",
+    options: [
+      "Good communication with others",
+      "Arrogance",
+      "At least 4hours of coding and learning every day",
+      "Adapting and adjusting to new emerging technologies to furnish their skillset",
+    ],
+  },
+
+  {
+    numb: 10,
+    question:
+      "Which of the following cannot be used to check various conditions in Javascript?",
+    answer: "None of the above",
+    options: [
+      "Ternary Statement",
+      "If...and else... statement",
+      "Switch statement",
+      "None of the above",
+    ],
+  },
+];
